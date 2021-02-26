e9e95d1dfcccbf9d271165bbbb04c2d137a00537
Kawsar-H <kawsarhossainkaiyes100u@gmail.com>
2021-02-26 16:51:41 +0600

    redesign

---
+++ src/component/Product/Product.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee , faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css'
+
+const Product = (props) => {
+    //console.log(props);
+    const{img , name , seller , price , stock} = props.product;
+    return (
+        <div className="product">
+            <div className="Pimg">
+             <img src={img} alt=""/>
+            </div>
+            <div className="product-name">
+                <br/>
+                <p><small>by {seller}</small></p>
+                <p>${price}</p>
+                <p><small>Only {stock} left in stock -soon buy</small></p>
+                <h4>{name}</h4>
+                <button
+                   className="main-button"
+                   onClick={() => props.handleAddProduct(props.product)}
+                   >
+                    
+                    <FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Product;
